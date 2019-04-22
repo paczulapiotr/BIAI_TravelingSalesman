205b6523a81391eed9ebd3d4e0327035a0e17fae
paczulapiotr <paczulapiotr@gmail.com>
2019-04-23 00:45:25 +0200

    Update input settings handling

--- src/components/panels/settingsPanel/index.js
+++ src/components/panels/settingsPanel/index.js
@@ -15,6 +15,13 @@ const maxValues = {
   pointsAmount: 100,
 };
 
+const minValues = {
+  width: 200,
+  height: 200,
+  population: 10,
+  pointsAmount: 3,
+};
+
 export default class SettingsPanel extends Component {
   static propTypes = {
     options: PropTypes.objectOf(PropTypes.any).isRequired,
@@ -24,6 +31,7 @@ export default class SettingsPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      shouldSave: false,
       populationSize: props.options.populationSize,
       mutation: props.options.mutation,
       width: props.options.width,
@@ -35,21 +43,36 @@ export default class SettingsPanel extends Component {
     };
   }
 
+handleShouldSave=() => {
+  const { shouldSave } = this.state;
+  if (!shouldSave) {
+    this.setState({ shouldSave: true });
+  }
+}
 
 handlePopulationSize=(e) => {
-  const val = Number.parseInt(e.target.value);
-  if (val > 0 && val < maxValues.population) {
+  this.handleShouldSave();
+  const { value } = e.target;
+  const val = Number.parseInt(value);
+  if (val >= 0 && val <= maxValues.population) {
     this.setState({ populationSize: val });
   } else if (val > maxValues.population) {
     this.setState({ populationSize: maxValues.population });
+  } else if (value === ''
+  || value === null
+  || value === undefined) {
+    this.setState({ populationSize: 0 });
   }
 }
 
 handleMutation=(e) => {
+  this.handleShouldSave();
   const { value } = e.target;
   const val = Number.parseFloat(value);
   if (val >= 0 && val < 100) {
     this.setState({ mutation: val });
+  } else if (val >= 100) {
+    this.setState({ mutation: 99 });
   } else if (value === ''
   || value === null
   || value === undefined) {
@@ -58,37 +81,57 @@ handleMutation=(e) => {
 }
 
 handleWidth=(e) => {
-  const val = Number.parseInt(e.target.value);
-  if (val > 0 && val < maxValues.width) {
+  this.handleShouldSave();
+  const { value } = e.target;
+  const val = Number.parseInt(value);
+  if (val >= 0 && val <= maxValues.width) {
     this.setState({ width: val, points: [] });
   } else if (val > maxValues.width) {
     this.setState({ width: maxValues.width, points: [] });
+  } else if (value === ''
+  || value === null
+  || value === undefined) {
+    this.setState({ width: 0 });
   }
 }
 
 handleHeight=(e) => {
-  const val = Number.parseInt(e.target.value);
-  if (val > 0 && val < maxValues.height) {
+  this.handleShouldSave();
+  const { value } = e.target;
+  const val = Number.parseInt(value);
+  if (val >= 0 && val <= maxValues.height) {
     this.setState({ height: val, points: [] });
   } else if (val > maxValues.height) {
     this.setState({ height: maxValues.height, points: [] });
+  } else if (value === ''
+  || value === null
+  || value === undefined) {
+    this.setState({ height: 0 });
   }
 }
 
 handleAmount=(e) => {
-  const val = Number.parseInt(e.target.value);
-  if (val > 0 && val < maxValues.height) {
+  this.handleShouldSave();
+  const { value } = e.target;
+  const val = Number.parseInt(value);
+  if (val >= 0 && val <= maxValues.pointsAmount) {
     this.setState({ pointsAmount: val, points: [] });
-  } else if (val > maxValues.height) {
+  } else if (val > maxValues.pointsAmount) {
     this.setState({ pointsAmount: maxValues.pointsAmount, points: [] });
+  } else if (value === ''
+  || value === null
+  || value === undefined) {
+    this.setState({ pointsAmount: 0 });
   }
 }
 
 handleCheckbox=(e) => {
+  this.handleShouldSave();
   this.setState({ showPopulationsBest: e.target.checked });
 }
 
 handleRandomPoints=(e) => {
+  this.handleShouldSave();
   this.setState({ randomPoints: e.target.checked });
 }
 
@@ -106,19 +149,49 @@ handleReset=() => {
 }
 
 handleSave=() => {
+  this.setState({ shouldSave: false });
   const { updateOptions } = this.props;
-  const {
+  let {
     populationSize,
-    mutation,
     width,
     height,
+    pointsAmount,
+  } = this.state;
+
+  const {
+    mutation,
     points,
     randomPoints,
-    pointsAmount,
     showPopulationsBest,
   } = this.state;
-  const canvasHeight = showPopulationsBest ? height * 2 : height;
+
+  if (height < minValues.height) {
+    height = minValues.height; // eslint-disable-line
+    this.setState({ height });
+  }
+
+  const canvasHeight = showPopulationsBest
+    ? height * 2
+    : height;
+
+  if (width < minValues.width) {
+    width = minValues.width;// eslint-disable-line
+    this.setState({ width });
+  }
+
   const canvasWidth = width;
+
+  if (pointsAmount < minValues.pointsAmount) {
+    pointsAmount = minValues.pointsAmount; // eslint-disable-line
+    this.setState({ pointsAmount });
+  }
+
+  if (populationSize < minValues.population) {
+    populationSize = minValues.population; // eslint-disable-line
+    this.setState({ populationSize });
+  }
+
+
   updateOptions({
     populationSize,
     mutation,
@@ -138,11 +211,13 @@ handleManualSelector=(points) => {
     points,
     pointsAmount: points.length,
     randomPoints: false,
+    shouldSave: true,
   });
 }
 
 render() {
   const {
+    shouldSave,
     populationSize,
     mutation,
     width,
@@ -188,7 +263,7 @@ render() {
           />
           <footer className="settings-buttons">
             <Button variant="outlined" onClick={this.handleReset} color="primary">Reset</Button>
-            <Button variant="outlined" onClick={this.handleSave} color="primary">Save</Button>
+            <Button variant={shouldSave ? 'contained' : 'outlined'} onClick={this.handleSave} color="primary">Save</Button>
           </footer>
         </FormControl>
       </ExpansionPanelDetails>
